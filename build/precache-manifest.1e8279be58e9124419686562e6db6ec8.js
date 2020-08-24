0a86db52dc0f237a7998740f39a1766ba14204c5
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-08-24 06:02:41 -0400

    installing gh-pages, etc.

---
+++ build/precache-manifest.1e8279be58e9124419686562e6db6ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ac1633380d6baa285e858d8e4f4825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbde9e053da298eb0239",
+    "url": "/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "bbde9e053da298eb0239",
+    "url": "/static/js/main.69318194.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
